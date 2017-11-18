b6845f173dadc093b88407f9ddc772c0ac358ff7
eyarijk <eyarij@gmail.com>
2017-11-18 21:36:52 +0200

    New style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,6 @@
 
 @section('content')
 <div class="container-fluid">
-  <div class="jumbotron">
-    <h1 class="display-3">Welcome</h1>
-    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <p><a class="btn btn-primary btn-lg" href="#">Learn more</a></p>
-  </div>
   <div class="row">
     <div class="col-md-3">
       <button type="button" class="btn btn-success btn-block">Ask a question</button><br>
